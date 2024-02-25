2f94c402d090efa6b200ecaa98ada0fb75898092
thatsaphon2745 <157302375+thatsaphon2745@users.noreply.github.com>
2024-02-25 16:23:37 +0700

    Debug & Bar guage & point in Overview

--- src/screens/pet/GameQuest.js
+++ src/screens/pet/GameQuest.js
@@ -11,23 +11,20 @@ import { setItemCategory } from "../../redux/variableSlice";
 export const GameQuest = ({navigation})=>{
     const dispatch = useDispatch();
 
-    dispatch(setItemCategory(''))
 
     const user = useSelector((state)=>state.auths);
     const userUID = user[0].uid;
 
     const isUpdate = useSelector((state)=>state.variables.isUpdate);
-
+    
     const [incomeAndExpensesDataSelected, setIncomeAndExpensesDataSelected] = useState({})
-
-    //dispatch ถูกใช้เพื่อ dispatch action เมื่อค่า isUpdate เปลี่ยนแปลง เพื่อทำให้ฟังก์ชัน getQuestData 
-    //ถูกเรียกใช้ใหม่เมื่อมีการอัปเดตข้อมูลที่เกี่ยวข้อง เช่น ข้อมูลของ Quest หรือข้อมูลอื่น ๆ 
-    //ซึ่งอาจมีการเปลี่ยนแปลงเมื่อมีการเพิ่มหรือลบข้อมูล หรืออื่น ๆ ที่ทำให้ข้อมูลที่แสดงในหน้าจอต้องถูกอัปเดตใหม่
+    useEffect(()=>{
+      dispatch(setItemCategory(''))
+    },[])
     useEffect(() => {
-      getQuestData(userUID);
-    }, [dispatch,isUpdate]);
-    //แก้ bug โดยการเพิ่มให้ getQuestDate รับ parameter เป็น userUID เพิ่ม แก้ config component warning
-  const getQuestData = async(userUID)=>{
+      getQuestData()
+    }, [isUpdate]);
+  const getQuestData = async()=>{
       try{
           const itemAllDataIncomeAndExpenses = await retrieveAllDataQuest(userUID)
           setIncomeAndExpensesDataSelected(itemAllDataIncomeAndExpenses)
